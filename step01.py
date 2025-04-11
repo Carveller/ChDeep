8560f750d87ec32be103d5718b64668eb50ffa30
cjq <chengjq0705@gmail.com>
2025-04-12 00:00:52 +0100

    数值微分

--- step01.py
+++ step01.py
@@ -22,6 +22,12 @@ class Exp(Function):
     def forward(self, x):
         return np.exp(x)
     
+def numerical_diff(f, x, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2 * eps)
 
 if __name__ == "__main__":
     A = Square()
@@ -35,3 +41,15 @@ if __name__ == "__main__":
     
     print(type(y))
     print(y.data)
+    
+    f = Square()
+    x = Variable(np.array(2.0))
+    dy = numerical_diff(f, x)
+    print(dy)
+    
+    def f(x):
+        return C(B(A(x)))
+    
+    x = Variable(np.array(0.5))
+    dy = numerical_diff(f, x)
+    print(dy)
