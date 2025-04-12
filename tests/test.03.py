a9bf9ed3bcf5b13fd008e8749d75f005d1f31426
cjq <chengjq0705@gmail.com>
2025-04-12 21:37:44 +0100

    测试square和exp

--- tests/test.03.py
+++ tests/test.03.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from core import Variable, Add, add
+from core import Variable, Add, add, square, exp
 
 # xs = [Variable(np.array(2)), Variable(np.array(3))]
 # f = Add()
@@ -14,4 +14,12 @@ from core import Variable, Add, add
 x0 = Variable(np.array(2))
 x1 = Variable(np.array(3))
 y = add(x0, x1)
+print(y.data)
+
+x3 = Variable(np.array(4))
+y = square(x3)
+print(y.data)
+
+x4 = Variable(np.array(5))
+y = exp(x4)
 print(y.data)
