92663d7b41e98e5fba8be4df254f9be6c1e83fb8
cjq <chengjq0705@gmail.com>
2025-05-16 01:33:42 +0100

    让变量更易用

--- core.py
+++ core.py
@@ -5,12 +5,13 @@ import contextlib
 from utils import as_array
 
 class Variable:
-    def __init__(self, data):
+    def __init__(self, data, name=None):
         if data is not None:
             if not isinstance(data, np.ndarray):
                 raise TypeError('{} is not supported'.format(type(data)))
         
         self.data = data
+        self.name = name
         self.grad = None
         self.creator = None
         # 增加辈分变量，实现复杂计算图
@@ -78,7 +79,31 @@ class Variable:
     
     def cleargrad(self):
         self.grad = None
+    
+    @property
+    def shape(self):
+        return self.data.shape
 
+    @property
+    def ndim(self):
+        return self.data.ndim
+    
+    @property
+    def size(self):
+        return self.data.size
+    
+    @property
+    def dtype(self):
+        return self.data.dtype
+    
+    def __len__(self):
+        return len(self.data)
+
+    def __repr__(self):
+        if self.data is None:
+            return 'variable(None)'
+        p = str(self.data).replace('\n', '\n' + ' ' * 9)
+        return 'variable(' + p + ')'
         
 class Function:
     def __call__(self, *inputs):
